ec253a5d876602bec17356e7f681f4960c71c39f
Richard-Persson <ricpersson00@gmail.com>
2025-03-09 14:00:55 +0100

    Endret på lagring av CNN modell

--- src/models/cnn_rgb.py
+++ src/models/cnn_rgb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -35,4 +36,21 @@ if __name__ == "__main__":
 
     model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
 
- 
+    y_pred = model.predict(X_test)
+
+    # If your ground truth labels (y_test) and predictions are one-hot encoded,
+    # convert them to integer labels using np.argmax.
+    if y_test.ndim > 1 and y_test.shape[1] > 1:
+        y_true_labels = np.argmax(y_test, axis=1)
+    else:
+        y_true_labels = y_test
+
+    if y_pred.ndim > 1 and y_pred.shape[1] > 1:
+        y_pred_labels = np.argmax(y_pred, axis=1)
+    else:
+        y_pred_labels = y_pred
+
+    np.save("models/CNN/y_true_rgb.npy", y_true_labels)
+    np.save("models/CNN/y_pred_rgb.npy", y_pred_labels)
+
+    model.save("models/CNN/landcover_cnn_rgb.h5")
