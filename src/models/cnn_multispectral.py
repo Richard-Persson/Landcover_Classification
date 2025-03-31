8f5d7fa498b03a7ce54d959cc4f99972c18bdd91
Richard-Persson <ricpersson00@gmail.com>
2025-03-31 15:56:36 +0200

    endret spectrale bånd

--- src/models/cnn_multispectral.py
+++ src/models/cnn_multispectral.py
@@ -12,6 +12,11 @@ from src.utils.data_loader import get_dataset, IMG_SIZE, NUM_CLASSES
 
 # Test med et bilde
 IMAGE = "data/raw/EuroSAT_MS/AnnualCrop/AnnualCrop_1.tif"
+BANDS = 3
+SWIR = 3
+NDVI = 2
+RGB = 1
+
 
 def build_model(input_shape, num_classes):
     """Bygger en sekvensiell CNN modell"""
@@ -25,24 +30,22 @@ def build_model(input_shape, num_classes):
         Dropout(0.5),
         Dense(num_classes, activation='softmax')
     ])
-    
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
 # Deler opp data og trener modellen på MS båndene
 if __name__ == "__main__":
-    # TODO fikse get_dataset for multispectral bilder
-    X_train, X_test, y_train, y_test = get_dataset(data_type="MS")
+    X_train, X_test, y_train, y_test = get_dataset(data_type="MS", bands=SWIR)
 
     print('MAIN: MS')
-    model = build_model((IMG_SIZE, IMG_SIZE, 13), NUM_CLASSES)
+    model = build_model((IMG_SIZE, IMG_SIZE, BANDS), NUM_CLASSES)
     model.summary()
     model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
 
     y_pred = model.predict(X_test)
 
-    np.save("models/CNN/y_true_ms.npy", y_test)
-    np.save("models/CNN/y_pred_ms.npy", y_pred)
+    np.save("models/CNN/y_true_ms_SWIR.npy", y_test)
+    np.save("models/CNN/y_pred_ms_SWIR.npy", y_pred)
 
-    model.save("models/CNN/landcover_ms.keras")
+    model.save("models/CNN/landcover_ms_SWIR.keras")
